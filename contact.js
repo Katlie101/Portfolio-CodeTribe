9faaaba65626a579a3c219b2d49084a44fba5aa6
Katlego <Katlegom9@gmail.com>
2024-04-24 20:19:33 +0200

    update

---
+++ contact.js
@@ -0,0 +1,10 @@
+function handleFormSubmit (event) {
+
+    event.preventDefault()
+    console.log(event);
+
+    console.log(document)
+    const emailInput = document.getElementById ("email") 
+    const messageInput = document.getElementById ("message")
+
+}

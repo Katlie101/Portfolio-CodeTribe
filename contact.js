8dd6457cafa2fa53bd1e297752a8da854342616f
Katlego <Katlegom9@gmail.com>
2024-04-25 03:08:39 +0200

    update

--- contact.js
+++ contact.js
@@ -1,10 +1,25 @@
 function handleFormSubmit (event) {
 
     event.preventDefault()
-    console.log(event);
+   // console.log(event);
 
-    console.log(document)
+    //console.log(document)
     const emailInput = document.getElementById ("email") 
     const messageInput = document.getElementById ("message")
 
+    
+    //console.log(emailInput);
+    const isEmailValid = emailInput.value !== '' && emailInput.validity.typeMismatch
+    
+    console.log(isEmailValid);
+
+    let isMessageValid = false;
+     if (messageInput.value !== '') {
+        isMessageValid = true
+     } else {
+        isMessageValid = false
+     }
+
+     console.log ({isMessageValid})
+   
 }
